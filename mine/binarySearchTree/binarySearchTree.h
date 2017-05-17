f79ba08d300b0e1eed74d28b828757a7b92e13bd
fwar34 <fwar34@126.com>
2017-05-17 22:44:29 +0800

    add binary search tree

---
+++ mine/binarySearchTree/binarySearchTree.h
@@ -0,0 +1,27 @@
+/*************************************************************************
+    > File Name: binarySearchTree.h
+    > Author: Feng
+    > Created Time: 2017年05月17日 星期三 21时34分51秒
+    > Content: 二叉搜索树
+ ************************************************************************/
+#pragma once
+
+template <typename K>
+struct bsnode
+{
+	bsnode* left;
+	bsnode* right;
+	K key;
+};
+
+template <typename K>
+class bstree
+{
+private:
+	bsnode<K>* m_root;
+
+public:
+	void insert(K key);
+	void del(K key);
+	bsnode<K>* search(K key);
+};
