f903c39d2c9aaaa451fbcc615d07526f89b7cf48
fwar34 <fwar34@126.com>
2017-05-17 16:27:00 +0800

    add test file

---
+++ mine/rb/test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author: Feng
+    > Created Time: 2017年05月17日 星期三 16时24分58秒
+    > Content: 
+ ************************************************************************/
+
+#include <iostream>
+#include "rb.h"
+using namespace std;
+
+int main()
+{
+	return 0;
+}
