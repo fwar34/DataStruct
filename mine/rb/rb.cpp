18f761d733de32a604311a10bb1016eb8806d376
fwar34 <fwar34@126.com>
2017-05-17 21:33:41 +0800

    add rightrotate

--- mine/rb/rb.cpp
+++ mine/rb/rb.cpp
@@ -29,3 +29,32 @@ rbnode<K, V>* leftRotate(rbnode<K, V>* x)
 		return y;
 	}
 }
+
+template<typename K, typename V>
+rbnode<K, V>* rightRotate(rbnode<K, V>* x)
+{
+	if (!x)
+	{
+		return NULL;
+	}
+
+	rbnode<K, V>* y = x->left;
+	x->left = x->right;
+	y->right->parent = x;
+	y->right = x;
+	y->parent = x->parent;
+	if (x->parent->left == x)
+	{
+		x->parent->left = y;
+	}
+	else
+	{
+		x->parent->right = y;
+	}
+	x->parent = y;
+
+	if (!y->parent)
+	{
+		return y;
+	}
+}
