5014a1f3e3d7120b4a94ffc77e7a5b6ee16439a0
fwar34 <fwar34@126.com>
2017-06-01 16:27:11 +0800

    rb tree

--- mine/rbTree/rbTree.h
+++ mine/rbTree/rbTree.h
@@ -9,6 +9,10 @@
 
 #include <iostream>
 #include <algorithm>
+#include <queue>
+#include <vector>
+using namespace std;
+
 
 enum rbcolor
 {
@@ -40,15 +44,141 @@ public:
 	rbtree() : m_root(NULL), m_size(0)
 	{
 	}
+	rbnode<K>* root()
+	{
+		return m_root;
+	}
+	void printtree();
+	void levelout();
 	void insert(K key);
 	void remove(K key);
+	size_t size()
+	{
+		return m_size;
+	}
+	int height()
+	{
+		return height(m_root);
+	}
+	void PrintBinaryTree();
 
 private:
 	rbnode<K>* grandparent(rbnode<K>* node);
 	rbnode<K>* uncle(rbnode<K>* node);
 	void llrotate(rbnode<K>* node);
 	void rrrotate(rbnode<K>* node);
+	void printtree(rbnode<K>* tree, int height, int totalheight);
+	int height(rbnode<K>* tree);
+	void PrintNode(vector<rbnode<K>*>& nodes, int level, int max_level);
+	void PrintWhiteSpaces(int num);
+	bool IsAllElementsNULL(const vector<rbnode<K>*> &nodes);
 };
+
+template <typename K>
+void rbtree<K>::levelout()
+{
+	int totalheight = height(m_root);
+	size_t curcount = 1;
+	size_t nextcount = 0;
+	std::queue<rbnode<K>*> q;
+	rbnode<K>* node = NULL;
+
+	if (!m_root)
+	{
+		return;
+	}
+	
+	q.push(m_root);
+	while (!q.empty())
+	{
+		for (int i = 0; i < totalheight; ++i)
+		{
+			std::cout << " ";
+		}
+		totalheight--;
+		
+		node = q.front();
+		q.pop();
+		std::cout << node->key << "," << (node->color == RED ? "R" : "B");
+		for (int i = 0; i < totalheight; ++i)
+		{
+			cout << " ";
+		}
+		
+		curcount--;
+
+		if (node->left)
+		{
+			q.push(node->left);
+			nextcount++;
+		}
+		
+		if (node->right)
+		{
+			q.push(node->right);
+			nextcount++;
+		}
+		
+		if (curcount == 0)
+		{
+			curcount = nextcount;
+			nextcount = 0;
+			std::cout << std::endl;
+		}
+	}
+}
+
+template <typename K>
+int rbtree<K>::height(rbnode<K>* tree)
+{
+	if (!tree)
+	{
+		return 0;
+	}
+	
+	int left = height(tree->left);
+	int right = height(tree->right);
+
+	return left > right ? left + 1 : right + 1;
+}
+
+template <typename K>
+void rbtree<K>::printtree()
+{
+	int total = height(m_root);
+	for (int i = 0, j = total; i <= total; ++i, j--)
+	{
+		for (int count = 0; count < j; ++count)
+		{
+			std::cout << "   ";
+		}
+		
+		printtree(m_root, i, total);
+		std::cout << std::endl;
+	}
+}
+
+template <typename K>
+void rbtree<K>::printtree(rbnode<K>* tree, int height, int totalheight)
+{
+	if (!tree || height < 0)
+	{
+		return;
+	}
+	
+	if (height == 0)
+	{
+		std::cout << tree->key << "," << (tree->color == RED ? "R" : "B");
+		for (int i = 0; i < totalheight - height; ++i)
+		{
+			std::cout << " ";
+		}
+		return;
+	}
+	
+	printtree(tree->left, height - 1, totalheight);
+	printtree(tree->right, height - 1, totalheight);
+}
 
 template <typename K>
 rbnode<K>* rbtree<K>::grandparent(rbnode<K>* node)
@@ -74,15 +204,27 @@ void rbtree<K>::llrotate(rbnode<K>* node)
 {
 	rbnode<K>* x = node->left;
 	node->left = x->right;
-	node->left->parent = node;
+	if (node->left)
+	{
+		node->left->parent = node;
+	}
+	
 	x->right = node;
 	if (node == m_root)
 	{
-		m_root == x;
+		m_root = x;
 		x->parent = NULL;
 	}
 	else
 	{
+		if (node == node->parent->left)
+		{
+			node->parent->left = x;
+		}
+		else
+		{
+			node->parent->right = x;
+		}
 		x->parent = node->parent;
 	}
 	node->parent = x;
@@ -93,7 +235,10 @@ void rbtree<K>::rrrotate(rbnode<K>* node)
 {
 	rbnode<K>* x = node->right;
 	node->right = x->left;
-	node->right->parent = node;
+	if (node->right)
+	{
+		node->right->parent = node;
+	}
 	x->left = node;
 	if (node == m_root)
 	{
@@ -102,6 +247,14 @@ void rbtree<K>::rrrotate(rbnode<K>* node)
 	}
 	else
 	{
+		if (node == node->parent->left)
+		{
+			node->parent->left = x;
+		}
+		else
+		{
+			node->parent->right = x;
+		}
 		x->parent = node->parent;
 	}
 	node->parent = x;
@@ -125,6 +278,13 @@ void rbtree<K>::insert(K key)
 		}
 	}
 
+	//有相同的key直接返回
+	if (dest)
+	{
+		std::cout << "exist key " << key << std::endl;
+		return;
+	}
+
 	if (!parentdest)
 	{
 		m_root = new rbnode<K>(key);
@@ -145,7 +305,6 @@ void rbtree<K>::insert(K key)
 	dest->parent = parentdest;
 	m_size++;
 	
-
 	while (true)
 	{
 		//case1 空树
@@ -170,7 +329,7 @@ void rbtree<K>::insert(K key)
 				grandparent(dest)->color = RED;
 				dest = grandparent(dest);
 			}
-			else
+			else	//XYb
 			{
 				if (dest->parent == grandparent(dest)->left)
 				{
@@ -180,6 +339,7 @@ void rbtree<K>::insert(K key)
 						dest->parent->color = BLACK;
 						grandparent(dest)->color = RED;
 						llrotate(grandparent(dest));
+						return;
 					}
 					else	//case4 LRb
 					{
@@ -200,12 +360,125 @@ void rbtree<K>::insert(K key)
 						 dest->parent->color = BLACK;
 						 grandparent(dest)->color = RED;
 						 rrrotate(grandparent(dest));
+						 return;
 					}
 				}
 			}
 		}
-	}
-	
-	
-		
+	}	
 }
+
+
+//////////////////////////////////////////////////////////////////////////
+template <typename K>
+void rbtree<K>::PrintWhiteSpaces(int num)  
+{  
+	for (int i = 0; i < num; ++i)
+	{
+		cout << " ";  
+	}
+}  
+
+template <typename K>
+void rbtree<K>::PrintNode(vector<rbnode<K>*>& nodes, int level, int max_level)  
+{  
+	if (nodes.empty() || IsAllElementsNULL(nodes)) 
+	{
+		return; // exit  
+	}
+
+	int floor = max_level - level;  
+	int endge_lines = 1 << (std::max(floor - 1, 0));  
+	int first_spaces = (1 << floor) - 1;  
+	int between_spaces = (1 << (floor+1)) - 1;  
+
+	PrintWhiteSpaces(first_spaces);  
+
+	// print the 'level' level   
+	vector<rbnode<K>*> new_nodes;  
+	typename::vector<rbnode<K>*>::const_iterator it = nodes.begin();  
+	for (; it != nodes.end(); ++it)
+	{  
+		if (*it != NULL)
+		{  
+			cout << (*it)->key;  
+			new_nodes.push_back((*it)->left);  
+			new_nodes.push_back((*it)->right);  
+		}  
+		else
+		{  
+			new_nodes.push_back(NULL);  
+			new_nodes.push_back(NULL);  
+			cout << " ";  
+		}  
+		PrintWhiteSpaces(between_spaces);  
+	}  
+	cout << endl;  
+
+	// print the following /s and \s  
+	for (int i = 1; i <= endge_lines; ++i)
+	{  
+		for(int j = 0; j < nodes.size(); ++j)
+		{  
+			PrintWhiteSpaces(first_spaces - i);  
+			if (nodes[j] == NULL)
+			{  
+				PrintWhiteSpaces(endge_lines + endge_lines + i + 1);  
+				continue;  
+			}  
+			if (nodes[j]->left != NULL)  
+			{
+				cout << "/";  
+			}
+			else  
+			{
+				PrintWhiteSpaces(1);  
+			}
+
+			PrintWhiteSpaces(i + i - 1);  
+
+			if (nodes[j]->right != NULL)  
+			{
+				cout << "\\";  
+			}
+			else  
+			{
+				PrintWhiteSpaces(1);  
+			}
+
+			PrintWhiteSpaces(endge_lines + endge_lines - i);  
+		}  
+		cout << endl;  
+	}  
+
+	PrintNode(new_nodes, level + 1, max_level);  
+
+}  
+
+// test whether all elements in vector are NULL  
+template <typename K>
+bool rbtree<K>::IsAllElementsNULL(const vector<rbnode<K>*> &nodes)  
+{  
+	typename::vector<rbnode<K>*>::const_iterator it = nodes.begin();  
+
+	while (it != nodes.end())
+	{  
+		if (*it) 
+		{
+			return false;   
+		}
+		++it;  
+	}  
+	return true;  
+}  
+
+template <typename K>
+void rbtree<K>::PrintBinaryTree()  
+{  
+	int max_level = height(m_root);  
+	vector<rbnode<K>*> nodes;  
+
+	nodes.push_back(m_root);  
+
+	PrintNode(nodes, 1, max_level);  
+}  
--- mine/rbTree/test.cpp
+++ mine/rbTree/test.cpp
@@ -5,6 +5,7 @@
     > Content: 
  ************************************************************************/
 
+#include <sys/time.h>
 #include <iostream>
 #include "rbTree.h"
 
@@ -13,11 +14,33 @@ using namespace std;
 int main()
 {
 	rbtree<int> tree;
+	srand(9999);
 
-	tree.insert(33);
-	tree.insert(111);
-	tree.insert(3234);
+	/*int* array = new int[11];
+	for (int i = 0; i < 11; ++i)
+	{
+		array[i] = rand();
+	}*/
 
+	int array[16] = {10, 7, 3, 18, 3, 9, -1, 98, 0, 83, 1, 100, -87, 33, 21, 12};
+	
+	cout << "-------------------begin1-----------------" << endl;
+	struct timeval tv1, tv2;
+	gettimeofday(&tv1, NULL);
+	for (int i = 0; i < 16; ++i)
+	{
+		tree.insert(array[i]);
+		tree.printtree();
+		cout << "----------------------------------------" << endl;
+		tree.levelout();
+		cout << "-----------------endl-------------------" << endl;
+	}
+	gettimeofday(&tv2, NULL);
+	cout << "------------------insert complete, time----------------------- " 
+		<< tv2.tv_sec * 1000000 + tv2.tv_usec - tv1.tv_sec * 1000000 - tv1.tv_usec << endl;
+	cout << "size  = " << tree.size() << " height = " << tree.height() << endl;	
+
+	tree.PrintBinaryTree();
 
 	return 0;
 }
