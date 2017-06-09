e0aca24e6ef65461929eecbd0b1c4844273a107e
fwar34 <fwar34@126.com>
2017-06-09 23:10:47 +0800

    btree

---
+++ mine/btree/btree.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+    > File Name: btree.h
+    > Author: Feng
+    > Created Time: 2017年06月09日 星期五 23时05分41秒
+    > Content: btree
+ ************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+template <typename K>
+struct bnode{
+
+};
+
+class btree
+{
+    
+}
---
+++ mine/btree/test.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author: Feng
+    > Created Time: 2017年06月09日 星期五 23时07分32秒
+    > Content:
+ ************************************************************************/
+#include "btree.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    btree<int> tree;
+    
+
+    return 0;
+}
