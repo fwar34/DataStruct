8ce5e3e1d74fbef34163616f949697b6c7866939
fwar34 <fwar34@126.com>
2018-12-24 18:41:26 +0800

    add rust

--- mine/sorts/minheap.rs
+++ mine/sorts/minheap.rs
@@ -5,13 +5,20 @@ struct MinHeap<T> {
 }
 
 impl<T: Copy + PartialOrd + Display> MinHeap<T> {
-    fn new(heap: &Vec<T>) -> MinHeap<T> {
-        MinHeap {
-            heap: heap,
+    fn new(heap: &[T]) -> MinHeap<T> {
+        let minheap = MinHeap {
+            heap: Vec::new(),
+        };
+
+        minheap.heap.push(-1);
+        for i in heap {
+            minheap.heap.push(*i);
         }
+
+        minheap
     }
 
-    fn new_c(capacity: usize) -> MinHeap<T> {
+    fn new2(capacity: usize) -> MinHeap<T> {
         MinHeap {
             heap: Vec::with_capacity(capacity),
         }
@@ -29,12 +36,32 @@ impl<T: Copy + PartialOrd + Display> MinHeap<T> {
 
     }
 
-    fn sink(&self, i: i32) {
+    fn sink(&mut self, mut i: usize) {
+        let t: T = self.heap[i].clone();
+        while 2 * i <= self.heap.len() { // node i have leaf
+            let mut child = 2 * i;
+            // node i have right leaf and right leaf is less than left leaf
+            if child < self.heap.len() && self.heap[child] > self.heap[child + 1] { 
+                child = child + 1;
+            }
 
+            if &self.heap[i] > &self.heap[child] {
+                self.heap[i] = self.heap[child].clone();
+                i = child;
+            } else {
+                break;
+            }
+        }
+        self.heap[i] = t;
     }
 
-    fn swim(&self, i: i32) {
-
+    fn swim(&mut self, mut i: usize) {
+        let t: T = self.heap[i].clone();
+        while i > 1 && &self.heap[i / 2] < &self.heap[i] {
+            self.heap[i] = self.heap[i / 2].clone();
+            i = i / 2;
+        }
+        self.heap[i] = t;
     }
 
     fn dump(&self) {
@@ -51,5 +78,6 @@ impl<T: Copy + PartialOrd + Display> MinHeap<T> {
 fn main() {
     let heap = MinHeap { heap: vec![100, -5, 9, 0, -108, 33] };
     let heap = MinHeap::new(&vec![100, -5, 9, 0, -108, 33]);
+    let heap = MinHeap::new(&[100, -5, 9, 0, -108, 33]);
     heap.dump();
 }
